762798845dfd0ee097b61676648e83cfc30e9765
n♡user <189495611+Plovchikdeval@users.noreply.github.com>
2025-09-06 18:31:11 +0500

    Update configurator.py

--- configurator.py
+++ configurator.py
@@ -7,7 +7,10 @@ config_id = "2040"
 config_hash = "b18441a1ff607e10a989891a5462e627"
 config_model = "FoxUserbot"
 
-PATH_FILE = "userdata/config.ini"
+use_data_dir = 'SHARKHOST' in os.environ or 'DOCKER' in os.environ
+base_dir = '/data' if use_data_dir else os.getcwd()
+userdata_dir = os.path.join(base_dir, "userdata")
+PATH_FILE = os.path.join(userdata_dir, "config.ini")
 
 config = configparser.ConfigParser()
 config.read(PATH_FILE)
@@ -18,7 +21,6 @@ def api():
     get_device_model = config.get("pyrogram", "device_model")
     return get_id, get_hash, get_device_model
 
-
 def update_api(api_id, api_hash):
     # old variable (not work)
     old_config_id = ["2860432", "29679445"]
@@ -27,32 +29,36 @@ def update_api(api_id, api_hash):
     config_changed = False
     
     if str(api_id) in old_config_id:
-        api_id_temp = api_id
         api_id = config_id
         config.set("pyrogram", "api_id", config_id)
         config_changed = True
-        print(f"Updated API ID from {api_id_temp} to {config_id}")
         
     if api_hash in old_config_hash:
-        api_hash_temp = api_hash
         api_hash = config_hash
         config.set("pyrogram", "api_hash", config_hash)
         config_changed = True
-        print(f"Updated API Hash from {api_hash_temp} to {config_hash}")
 
     if config_changed:
+        if not os.path.exists(userdata_dir):
+            try:
+                os.makedirs(userdata_dir)
+            except Exception:
+                pass
         with open(PATH_FILE, "w") as config_file:
             config.write(config_file)
-        print("Config file updated successfully!")
 
     return api_id, api_hash
-
 
 def my_api():
     try:
         api_id, api_hash, device_model = api()
         api_id, api_hash = update_api(api_id, api_hash)
     except (configparser.NoSectionError, configparser.NoOptionError):
+        if not os.path.exists(userdata_dir):
+            try:
+                os.makedirs(userdata_dir)
+            except Exception:
+                pass
         if not config.has_section("pyrogram"):
             config.add_section("pyrogram")
         
@@ -66,6 +72,5 @@ def my_api():
         api_id = config_id
         api_hash = config_hash
         device_model = config_model
-        print(f"Not found config.ini\nGenerating new...")
     
     return api_id, api_hash, device_model
