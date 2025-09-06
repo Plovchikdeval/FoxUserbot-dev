f9fda23081c56376c346a842df52c4599d932edd
n♡user <189495611+Plovchikdeval@users.noreply.github.com>
2025-09-06 20:10:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import re
 from requirements_installer import install_library
 from migrate import convert_modules
 
+use_data_dir = 'SHARKHOST' in os.environ or 'DOCKER' in os.environ
+
 def is_running_in_termux():
     termux_vars = [
         'TERMUX_VERSION',
@@ -17,7 +19,6 @@ def is_running_in_termux():
     return any(var in os.environ for var in termux_vars)
 
 def check_structure():
-    use_data_dir = 'SHARKHOST' in os.environ or 'DOCKER' in os.environ
     base_dir = '/data' if use_data_dir else os.getcwd()
 
     if use_data_dir and not os.path.exists(base_dir):
@@ -68,12 +69,15 @@ def autoupdater():
 async def start_userbot(app):
     await app.start()
     user = await app.get_me()
-    session_file = "my_account.session"
+    session_dir = '/data' if use_data_dir else os.getcwd()
+    session_file = os.path.join(session_dir, "my_account.session")
+    
     if os.path.exists(session_file):
         pass
     else:
-        if os.path.exists("localhost_run_output.txt"):
-            os.remove("localhost_run_output.txt")
+        localhost_file = os.path.join(session_dir, "localhost_run_output.txt")
+        if os.path.exists(localhost_file):
+            os.remove(localhost_file)
         os.execv(sys.executable, [sys.executable] + sys.argv)
 
 def userbot():
@@ -91,7 +95,6 @@ def userbot():
     
     api_id, api_hash, device_mod = my_api()
 
-    use_data_dir = 'SHARKHOST' in os.environ or 'DOCKER' in os.environ
     session_dir = '/data' if use_data_dir else os.getcwd()
 
     if use_data_dir and not os.path.exists(session_dir):
@@ -99,35 +102,35 @@ def userbot():
             os.makedirs(session_dir)
         except Exception:
             return
-
-    session_path = os.path.join(session_dir, "my_account.session")
     
-    if not os.path.exists(session_path):
-        if "--cli" in sys.argv:
-            client = Client(
-                session_path,
-                api_id=api_id,
-                api_hash=api_hash,
-                device_model=device_mod
-            )
-            client.run()
-        else:      
-            success, user = start_web_auth(api_id, api_hash, device_mod)
-            
-            if not success or user is None:
-                return
-            else:
-                if not os.path.exists(session_path):
-                    os.execv(sys.executable, [sys.executable] + sys.argv)
+    if "--cli" in sys.argv:
+        client = Client(
+            "my_account",
+            api_id=api_id,
+            api_hash=api_hash,
+            device_model=device_mod,
+            workdir=session_dir
+        )
+        client.run()
+    else:      
+        success, user = start_web_auth(api_id, api_hash, device_mod)
+        
+        if not success or user is None:
+            return
+        else:
+            session_file = os.path.join(session_dir, "my_account.session")
+            if not os.path.exists(session_file):
+                os.execv(sys.executable, [sys.executable] + sys.argv)
     
     prestart(api_id, api_hash, device_mod)
 
     try:
         client = Client(
-            session_path,
+            "my_account",
             api_id=api_id,
             api_hash=api_hash,
             device_model=device_mod,
+            workdir=session_dir,
             plugins=dict(root="modules" if not safe_mode else "modules/plugins_1system")
         )
         client.run()
